655889d053010c7c6b1ca50eb7ea8a84c1ad0e57
bishwajeetmodi <bishwajeetmodi0185@gmail.com>
2026-05-19 15:06:43 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,30 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import hashlib
 
-KEYWORDS = [
-    "phd", "ph.d", "doctoral",
-    "research admission",
-    "phd admission",
-    "fellowship",
-    "doctor of philosophy", 
-    "2026", 
-    "PhD Admission", 
-    "doctral programme", 
-    "2026-2027",
-    "UGC–JRF",
-    "PhD Admissions 2025-26",
-    "Ph.D. July, 2026 Session",
-    "Ph.D Admission Notification"
-]
+from pdfminer.high_level import extract_text
 
-def check_updates(url):
+
+KEYWORDS = ["phd", "ph.d", "doctoral", "research", "phd admission", "fellowship", "doctor of philosophy", 
+    "2026", "PhD Admission", "doctral programme", "2026-2027","UGC–JRF","PhD Admissions 2025-26", "Ph.D. July, 2026 Session",
+    "Ph.D Admission Notification"]
+
+def is_phd(text):
+    text = text.lower()
+    return any(k in text for k in KEYWORDS)
+
+
+def get_page_links(url):
     try:
         r = requests.get(url, timeout=10)
         soup = BeautifulSoup(r.text, "html.parser")
 
-        text = soup.get_text().lower()
+        links = []
+        for a in soup.find_all("a", href=True):
+            links.append(a["href"])
 
-        return any(keyword in text for keyword in KEYWORDS)
+        return links, soup.get_text()
 
     except:
-        return False
+        return [], ""
+
+
+def read_pdf(url):
+    try:
+        r = requests.get(url, timeout=10)
+        with open("temp.pdf", "wb") as f:
+            f.write(r.content)
+
+        text = extract_text("temp.pdf")
+        return text
+    except:
+        return ""
